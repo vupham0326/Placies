7c2662d0d26c0d08055ab85c0792226704c0760b
Vu Pham <vupham0326@mail.com>
2015-06-25 17:49:09 +0000

    adding the places a user created to their profile page

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,9 +2,9 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
   def comment_added(comment)
-	  @place = comment.place
-	  @place_owner = @place.user
-	  mail(to: @place_owner.email,
-       subject: "A comment has been added to #{@place.name}")
-end
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
+         subject: "A comment has been added to #{@place.name}")
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
-  belongs_to :user
+ belongs_to :user
   belongs_to :place
   after_create :send_comment_email
 
@@ -15,7 +15,7 @@ class Comment < ActiveRecord::Base
     RATINGS.invert[self.rating]
   end
 
-   def send_comment_email
-      NotificationMailer.comment_added(self).deliver
+  def send_comment_email
+    NotificationMailer.comment_added(self).deliver
   end
 end
